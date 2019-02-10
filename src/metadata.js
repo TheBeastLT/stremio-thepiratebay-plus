32715a218149105922816861a69e5db68f0572f6
TheBeastLT <pauliox@beyond.lt>
2019-02-10 10:59:35 +0100

    replace metadata cache with cache-manager memory cache

--- src/metadata.js
+++ src/metadata.js
@@ -1,26 +1,23 @@
 const imdb = require('imdb');
 const request = require('request');
+const cacheManager = require('cache-manager');
+
+const CINEMETA_URL = process.env.CINEMETA_URL || 'https://v3-cinemeta.strem.io';
+const KEY_PREFIX = 'streamio-ptb|metadata';
+const METADATA_TTL = process.env.METADATA_TTL || 2 * 24 * 60 * 60; // 2 days
+
+const cache = cacheManager.caching({store: 'memory', ttl: METADATA_TTL});
 
 module.exports.getMetadata = function (imdbId, type) {
-  return _getMetadataCache(imdbId)
-      .catch(err => _getMetadataCinemeta(imdbId, type))
-      .catch(err => _getMetadataImdb(imdbId))
-      .then(metadata => cache[imdbId] = metadata);
+  const key = `${KEY_PREFIX}:${imdbId}`;
+
+  return cache.wrap(key, function () {
+    return _getMetadataCinemeta(imdbId, type)
+        .catch(err => _getMetadataImdb(imdbId)); // fallback to imdb search
+  });
 };
 
-const cache = {};
-
-function _getMetadataCache(imdbId) {
-  return new Promise(function (resolve, rejected) {
-    if (cache[imdbId]) {
-      resolve(cache[imdbId]);
-    } else {
-      rejected(new Error("imdbId not cached"));
-    }
-  });
-}
-
-async function _getMetadataImdb(imdbId) {
+function _getMetadataImdb(imdbId) {
   return new Promise(function (resolve, rejected) {
     imdb(imdbId, function (err, data) {
       if (data) {
@@ -35,12 +32,10 @@ async function _getMetadataImdb(imdbId) {
   });
 }
 
-const cinemetaUrl = process.env.CINEMETA_URL || 'https://v3-cinemeta.strem.io';
-
-async function _getMetadataCinemeta(imdbId, type) {
+function _getMetadataCinemeta(imdbId, type) {
   return new Promise(function (resolve, rejected) {
     request(
-        `${cinemetaUrl}/meta/${type}/${imdbId}.json`,
+        `${CINEMETA_URL}/meta/${type}/${imdbId}.json`,
         (err, res, body) => {
           body = JSON.parse(body);
           if (body && body.meta && body.meta.name) {
