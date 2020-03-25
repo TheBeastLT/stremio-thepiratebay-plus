4878defab6ad35e1b102c60161905e27184e37ec
TheBeastLT <pauliox@beyond.lt>
2020-03-25 14:00:26 +0100

    setup Dockerfile

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const { serveHTTP, publishToCentral } = require('stremio-addon-sdk');
+const { serveHTTP } = require('stremio-addon-sdk');
 const addonInterface = require('./addon');
 
 const PORT = process.env.PORT || 7000;
-const ENDPOINT = process.env.ENDPOINT || `http://localhost:${PORT}`;
 
 serveHTTP(addonInterface, { port: PORT, cacheMaxAge: 86400, static: '/static' });
-publishToCentral(`${ENDPOINT}/manifest.json`);
